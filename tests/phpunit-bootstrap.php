b7c4735e4958fe07dd8c9e48a22fb2742f170ffc
Niels Keurentjes <niels.keurentjes@omines.com>
2017-11-02 02:10:39 +0100

    Upgrade PHPUnit bridge to get rid of external deprecation issues

--- tests/phpunit-bootstrap.php
+++ tests/phpunit-bootstrap.php
@@ -18,13 +18,6 @@ use Tests\Fixtures\AppKernel;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-// Polyfill PHPUnit 6.0 both ways
-if (!class_exists('\PHPUnit\Framework\TestCase', true)) {
-    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
-} elseif (!class_exists('\PHPUnit_Framework_TestCase', true)) {
-    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
-}
-
 // Clean up from previous runs
 @exec('rm -rf /tmp/datatables-bundle');
 
