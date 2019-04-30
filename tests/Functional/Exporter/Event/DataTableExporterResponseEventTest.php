e6c0e5a5e78b79ff0cef4b0517570f0ef49445e5
Maxime Pinot <contact@maximepinot.com>
2019-04-30 20:27:04 +0200

    Fix Content Disposition header format

--- tests/Functional/Exporter/Event/DataTableExporterResponseEventTest.php
+++ tests/Functional/Exporter/Event/DataTableExporterResponseEventTest.php
@@ -42,7 +42,12 @@ class DataTableExporterResponseEventTest extends WebTestCase
         /** @var BinaryFileResponse $response */
         $response = $this->client->getResponse();
 
-        static::assertSame('attachment; filename=custom_filename.' . $ext, $response->headers->get('content-disposition'));
+        $headers = [
+            sprintf('attachment; filename="custom_filename.%s"', $ext), // Symfony 3
+            sprintf('attachment; filename=custom_filename.%s', $ext),    // Symfony 4
+        ];
+
+        static::assertContains($response->headers->get('content-disposition'), $headers);
     }
 
     public function exporterNameProvider()
