b7c356d28d0ec070a4f339829df4bed364459193
Niels Keurentjes <niels.keurentjes@omines.com>
2019-08-09 14:16:06 +0200

    Fix code style

--- src/Adapter/ArrayAdapter.php
+++ src/Adapter/ArrayAdapter.php
@@ -46,16 +46,16 @@ class ArrayAdapter implements AdapterInterface
         // very basic implementation of sorting
         try {
             $oc = $state->getOrderBy()[0][0]->getName();
-            $oo = \strtolower($state->getOrderBy()[0][1]);
+            $oo = \mb_strtolower($state->getOrderBy()[0][1]);
 
             \usort($this->data, function ($a, $b) use ($oc, $oo) {
-                if ($oo === 'desc') {
+                if ('desc' === $oo) {
                     return $b[$oc] <=> $a[$oc];
                 }
 
                 return $a[$oc] <=> $b[$oc];
             });
-        }catch (\Throwable $exception) {
+        } catch (\Throwable $exception) {
             // ignore exception
         }
 
