107267d16bc3271085c68818e03200ad27a47b49
Maxime Pinot <contact@maximepinot.com>
2020-01-14 14:39:51 +0100

    Add CSV exporter

---
+++ src/Exporter/Csv/CsvExporter.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Omines\DataTablesBundle\Exporter\Csv;
+
+use Omines\DataTablesBundle\Exporter\DataTableExporterInterface;
+
+/**
+ * Exports DataTable data to a CSV file.
+ *
+ * @author Maxime Pinot <maxime.pinot@gbh.fr>
+ */
+class CsvExporter implements DataTableExporterInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function export(array $columnNames, \Iterator $data): \SplFileInfo
+    {
+        $filePath = sys_get_temp_dir() . '/' . uniqid('dt') . '.csv';
+
+        $file = fopen($filePath, 'w');
+
+        fputcsv($file, $columnNames);
+
+        foreach ($data as $row) {
+            fputcsv($file, array_map('strip_tags', $row));
+        }
+
+        fclose($file);
+
+        return new \SplFileInfo($filePath);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName(): string
+    {
+        return 'csv';
+    }
+}
