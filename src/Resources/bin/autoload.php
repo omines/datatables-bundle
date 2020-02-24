30527eb006549a92ec25374580a69ed43993db32
Niels Keurentjes <niels.keurentjes@omines.com>
2020-02-24 09:42:28 +0100

    Integrate translation update script

--- src/Resources/bin/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$autoload = __DIR__.'/../../../vendor/autoload.php';
-
-if (!file_exists($autoload)) {
-    echo 'You should run "composer install" in the library root before running this script.';
-}
-
-require_once $autoload;
