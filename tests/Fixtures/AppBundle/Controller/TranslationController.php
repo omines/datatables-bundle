23bbc42644f0624cd63b1f4ca5eef2074dbfe05f
freezy <freezy-sk@users.noreply.github.com>
2020-03-09 12:37:16 +0100

    Test local translations (#139)

---
+++ tests/Fixtures/AppBundle/Controller/TranslationController.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * Symfony DataTables Bundle
+ * (c) Omines Internetbureau B.V. - https://omines.nl/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Fixtures\AppBundle\Controller;
+
+use Omines\DataTablesBundle\Adapter\ArrayAdapter;
+use Omines\DataTablesBundle\Column\TextColumn;
+use Omines\DataTablesBundle\DataTableFactory;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * TranslationController.
+ */
+class TranslationController extends AbstractController
+{
+    public function tableAction(Request $request, DataTableFactory $dataTableFactory)
+    {
+        $datatable = $dataTableFactory->create();
+        $datatable
+            ->setName('noCDN')
+            ->setMethod(Request::METHOD_GET)
+            ->setLanguageFromCDN(false)
+            ->add('col3', TextColumn::class, ['label' => 'foo', 'field' => 'bar'])
+            ->add('col4', TextColumn::class, ['label' => 'bar', 'field' => 'foo'])
+            ->createAdapter(ArrayAdapter::class)
+        ;
+
+        if ($datatable->handleRequest($request)->isCallback()) {
+            return $datatable->getResponse();
+        }
+
+        return $this->render('@App/table.html.twig', [
+            'datatable' => $datatable,
+        ]);
+    }
+}
