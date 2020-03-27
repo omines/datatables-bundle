d63f3e20d40a0656d3a3fbf6cd48fdaa4b9034f7
Daniel Gorgan <danut007ro@gmail.com>
2020-03-27 13:59:02 +0200

    Remove scrollX

--- src/DataTable.php
+++ src/DataTable.php
@@ -51,7 +51,6 @@ class DataTable
         'orderCellsTop' => true,
         'stateSave' => false,
         'fixedHeader' => false,
-        'scrollX' => false,
     ];
 
     const DEFAULT_TEMPLATE = '@DataTables/datatable_html.html.twig';
