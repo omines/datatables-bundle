efe123cd177de243a155e14e1a61f28892330966
Maarten Visscher <maarten@clevermobi.com>
2024-03-05 10:43:37 +0100

    Basic OpenSpout Excel exporter

---
+++ src/Exporter/Excel/ExcelOpenSpoutExporter.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace Omines\DataTablesBundle\Exporter\Excel;
+
+use Omines\DataTablesBundle\Exporter\DataTableExporterInterface;
+use OpenSpout\Common\Entity\Row;
+use OpenSpout\Common\Entity\Style\Style;
+use OpenSpout\Writer\XLSX\Writer;
+
+/**
+ * Excel exporter using OpenShout.
+ */
+class ExcelOpenSpoutExporter implements DataTableExporterInterface
+{
+    public function export(array $columnNames, \Iterator $data): \SplFileInfo
+    {
+        $filePath = sys_get_temp_dir() . '/' . uniqid('dt') . '.xlsx';
+
+        $writer = new Writer();
+        $writer->openToFile($filePath);
+
+        // Write header
+        $boldStyle = (new Style())->setFontBold();
+        $writer->addRow(Row::fromValues($columnNames, $boldStyle));
+
+        // Write data
+        foreach ($data as $row) {
+            // Remove HTML tags
+            $values = array_map('strip_tags', $row);
+
+            $writer->addRow(Row::fromValues($values));
+        }
+
+        $writer->close();
+
+        return new \SplFileInfo($filePath);
+    }
+
+    public function getMimeType(): string
+    {
+        return 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+    }
+
+    public function getName(): string
+    {
+        return 'excel-openspout';
+    }
+}
